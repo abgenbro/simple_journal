49df0531d33d9d3cd1a6af99a647ddf18538aa5d
abgenbro <161126534+abgenbro@users.noreply.github.com>
2024-03-24 05:52:24 +0000

    added change

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request
-from database import get_entries, get_recent_entries, add_entry
-import datetime, time
+from database import get_entries, add_entry
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    now = datetime.datetime.now()
     if ('date' in request.form.keys()) and ('entry' in request.form.keys()):
-        add_entry(request.form['date'], request.form['entry'], int(time.mktime(now.timetuple())))
-    recent_entries = get_recent_entries()
+        add_entry(request.form['date'], request.form['entry'])
+    recent_entries = get_entries(5)
     return render_template("index.html", rec_ent=recent_entries)
+
+if __name__ == '__main__':
+    app.run(debug=True)
