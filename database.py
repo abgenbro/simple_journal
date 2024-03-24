7d810d9b1aa205e9eaca60ab4eb41edd250abdef
abgenbro <161126534+abgenbro@users.noreply.github.com>
2024-03-24 05:32:26 +0000

    Added changes

--- database.py
+++ database.py
@@ -1,34 +1,40 @@
 from sqlalchemy import create_engine, text
-import sqlite3, sys
+import sqlite3, sys, time
 
 engine = create_engine("sqlite:///entries.db")
 
 def make_entries_db():
     con = sqlite3.connect("entries.db")
     cur = con.cursor()
-    cur.execute("DROP TABLE if exists entries")
+    cur.execute("DROP TABLE IF EXISTS entries")
     mktable = """CREATE TABLE entries (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     date VARCHAR(10),
     entrytext VARCHAR(300),
     entrytime INTEGER)"""
     cur.execute(mktable)
 
-def get_entries():
+def get_entries(num=None, show=False):
     con = engine.connect()
-    return list(con.execute(text("SELECT * from entries")))
+    query_common = "SELECT * from entries"
+    query_num = '' if num == None else " ORDER BY entrytime DESC LIMIT {}".format(num)
+    query = query_common + query_num
+    res = list(con.execute(text(query)))
+    if show:
+        for r in res:
+            print(r)
+    return res
 
-def get_recent_entries():
-    con = engine.connect()
-    return list(con.execute(text("SELECT * from entries ORDER BY entrytime DESC LIMIT 5")))
-
-def add_entry(dt, etext, etime):
+def add_entry(dt, etext):
+    etime = int(time.time())
     con = engine.connect()
     ins = "INSERT INTO entries (date, entrytext, entrytime) VALUES (:dt, :etext, :etime)"
     con.execute(text(ins), {"dt": dt, "etext": etext, "etime": etime})
     con.commit()
 
-#add_entry("20240226", "hi hi bye", 123)
-
 if __name__ == '__main__':
     if '--make-entries' in sys.argv:
         make_entries_db()
+    if '--add' in sys.argv:
+        add_entry(sys.argv[1], sys.argv[2])
+    if '--show' in sys.argv:
+        get_entries(None, True)
